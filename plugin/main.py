237ea63820b03d590dd70531f1ccb2c1a14857a5
Garulf <535299+Garulf@users.noreply.github.com>
2021-07-16 09:29:02 -0400

    config is opened in init now

--- plugin/main.py
+++ plugin/main.py
@@ -11,9 +11,11 @@ from flowlauncher import FlowLauncher, FlowLauncherAPI
 MAX_RESULTS = 20
 SVG_FILE = '<?xml version="1.0" encoding="UTF-8"?><!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd"><svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" width="24" height="24" viewBox="0 0 24 24"><path d="{}" /></svg>'
 
+
 class MDI(FlowLauncher):
 
     def __init__(self):
+
         self.result = []
         with open("./icons.json", "r", "utf-8") as f:
             self.icons = json.load(f)
@@ -35,16 +37,13 @@ class MDI(FlowLauncher):
             }
         )
 
+
     def query(self, query):
-        f = codecs.open("./icons.json", "r", "utf-8")
-        icons = json.load(f)
-        f.close()
-        result = []
-        names = [icon['name'] for icon in icons['icons']]
+        # names = [icon['name'] for icon in icons['icons']]
         q = query.lower()
         
         if len(q) > 0:
-            for icon in icons['icons']:
+            for icon in self.icons['icons']:
                 # If only one char search by first letter only
                 if len(q) < 2 and icon['name'].startswith(q):
                     self.filter(icon)
@@ -60,6 +59,10 @@ class MDI(FlowLauncher):
                 }
             )
         return self.result
+
+
+    def test(self):
+        self.results = []
 
     def copy_to_clipboard(self, icon_name):
         win32clipboard.OpenClipboard()
