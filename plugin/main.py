0a51dc13e40840bf06f319d88a4d42e9c1e96540
Garulf <535299+Garulf@users.noreply.github.com>
2021-07-26 03:15:21 -0400

    remove limitations on results

--- plugin/main.py
+++ plugin/main.py
@@ -12,7 +12,7 @@ except ModuleNotFoundError:
     from flowlauncher import FlowLauncher
 
 ICON_FOLDER = './icons/'
-MAX_RESULTS = 20
+MAX_RESULTS = 100
 SVG_FILE = '<?xml version="1.0" encoding="UTF-8"?><!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd"><svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" width="24" height="24" viewBox="0 0 24 24"><path d="{}" /></svg>'
 MDI_URL = "https://materialdesignicons.com/icon/"
 
@@ -75,23 +75,17 @@ class MDI(FlowLauncher):
         # names = [icon['name'] for icon in icons['icons']]
         q = query.lower()
         
-        if len(q) > 0:
-            for icon in self.icons['icons']:
-                # If only one char search by first letter only
-                if len(q) < 2 and icon['name'].startswith(q):
-                    self.filter(icon)
-                elif len(q) > 1 and q in icon['name'] or q in icon['aliases']:
-                    self.filter(icon)
-                if len(self.results) >= MAX_RESULTS:
-                    break
-        else:
-            self.results.append(
-                {
-                    "Title": 'Please enter your search term',
-                    "SubTitle": '...'
-                }
-            )
-        return self.results[:10]
+
+        for icon in self.icons['icons']:
+            # If only one char search by first letter only
+            if len(q) < 2 and icon['name'].startswith(q):
+                self.filter(icon)
+            elif len(q) > 1 and q in icon['name'] or q in icon['aliases']:
+                self.filter(icon)
+            if len(self.results) >= MAX_RESULTS:
+                break
+
+        return self.results
 
 
     def copy_to_clipboard(self, icon_name):
