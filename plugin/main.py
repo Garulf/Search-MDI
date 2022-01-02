ed5fa8a3c068bba281e58730f2814566974b5501
Garulf <535299+Garulf@users.noreply.github.com>
2022-01-02 00:46:30 -0500

    Use font

--- plugin/main.py
+++ plugin/main.py
@@ -3,58 +3,67 @@ import json
 import os
 import webbrowser
 from pathlib import Path
+from difflib import SequenceMatcher as sm
 
 
-from flox import Flox, Clipboard
+from flox import Flox, Clipboard, ICON_BROWSER
 
 
 META_FILE = "meta.json"
-META_PATH = Path(Path.cwd(), META_FILE)
+META_PATH = Path(Path.cwd(), 'plugin', META_FILE)
 MAX_RESULTS = 100
 MDI_URL = "https://materialdesignicons.com/icon/"
 
 
-class MDI(Flox):
+class MDI(Flox, Clipboard):
     def __init__(self):
         with open(META_PATH, "r") as f:
             self.icons = json.load(f)
         super().__init__()
 
-    def filter(self, icon):
-
-        self.add_item(
-            title=icon["name"],
-            subtitle="Press ENTER to copy to clipboard (SHIFT+ENTER for more options)",
-            icon=self.create_icon(icon["name"]),
-            context=[icon["name"]],
-            method="copy_to_clipboard",
-            parameters=[icon["name"]],
-        )
-
     def context_menu(self, data):
+        name = data[0]
         self.add_item(
             title="View icon on website",
-            subtitle=f"{MDI_URL}{data[0]}",
-            icon=self.create_icon("web"),
+            subtitle=f"{MDI_URL}{name}",
+            icon=ICON_BROWSER,
             method="open_web",
             parameters=[data[0]],
         )
         return self._results
 
+    def codepoint_to_char(self, codepoint):
+        return chr(int(codepoint, 16))
+
+    def match(self, query, icon_name, aliases):
+        query = query.lower()
+        score = []
+        for alias in aliases:
+            _ = sm(lambda x: x==" " or x=="-", query, alias.lower())
+            score.append(_.ratio() * 100)
+        _ = sm(lambda x: x==" " or x=="-", query, icon_name.lower())
+        score.append(_.ratio() * 100)
+        return max(score)
+
     def query(self, query):
-        # names = [icon['name'] for icon in icons['icons']]
-        q = query.lower()
-
-        for icon in self.icons["icons"]:
-            self.add_item(
-                title=icon["name"],
-            )
-            
-
-        return self._results
-
-    def copy_to_clipboard(self, icon_name):
-        pyperclip.copy(icon_name)
+        for icon in self.icons:
+            score = self.match(query, icon["name"], icon['aliases'])
+            if score > 50 or query == '':
+                self.add_item(
+                    title=icon["name"],
+                    subtitle=", ".join(icon['aliases']),
+                    glyph=self.codepoint_to_char(icon["codepoint"]),
+                    font_family=str(
+                                Path(self.plugindir).joinpath(
+                                    'plugin',
+                                    "#Material Design Icons Desktop"
+                                )
+                    ),
+                    score=int(score),
+                    context=[icon["name"]],
+                    method=self.put,
+                    parameters=[icon["name"]],
+                )
 
     def open_web(self, icon_name):
         webbrowser.open(f"{MDI_URL}{icon_name}")
