863ca40305d49381ad7461e592e1fabcd6468816
Garulf <535299+Garulf@users.noreply.github.com>
2022-01-01 23:53:54 -0500

    Use new database

--- plugin/main.py
+++ plugin/main.py
@@ -2,32 +2,23 @@
 import json
 import os
 import webbrowser
+from pathlib import Path
 
 
 from flox import Flox, Clipboard
 
 
-ICON_FOLDER = "./icons/"
+META_FILE = "meta.json"
+META_PATH = Path(Path.cwd(), META_FILE)
 MAX_RESULTS = 100
-SVG_FILE = '<?xml version="1.0" encoding="UTF-8"?><!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd"><svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" width="24" height="24" viewBox="0 0 24 24"><path d="{}" /></svg>'
 MDI_URL = "https://materialdesignicons.com/icon/"
 
 
 class MDI(Flox):
     def __init__(self):
-        with open("./plugin/icons.json", "r") as f:
+        with open(META_PATH, "r") as f:
             self.icons = json.load(f)
-        if not os.path.exists(ICON_FOLDER):
-            os.mkdir(ICON_FOLDER)
         super().__init__()
-
-    def create_icon(self, icon_name):
-        if not os.path.isfile(f"{ICON_FOLDER}{icon_name}.svg"):
-            for icon in self.icons["icons"]:
-                if icon["name"] == icon_name:
-                    with open(f"{ICON_FOLDER}{icon['name']}.svg", "w") as f:
-                        f.write(SVG_FILE.format(icon["data"]))
-        return f"{ICON_FOLDER}{icon_name}.svg"
 
     def filter(self, icon):
 
@@ -55,13 +46,10 @@ class MDI(Flox):
         q = query.lower()
 
         for icon in self.icons["icons"]:
-            # If only one char search by first letter only
-            if len(q) < 2 and icon["name"].startswith(q):
-                self.filter(icon)
-            elif len(q) > 1 and q in icon["name"] or q in icon["aliases"]:
-                self.filter(icon)
-            if len(self._results) >= MAX_RESULTS:
-                break
+            self.add_item(
+                title=icon["name"],
+            )
+            
 
         return self._results
 
